fea26d84934332031d8079b1879aa5a35cc3ce86
Thalia Mijas <tlmijas@outlook.com>
2025-04-15 16:13:32 -0500

    Agregar componente NotFound

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import Header from "../Header/Header.jsx";
 import List from "../List/List.jsx";
 import Details from "../Details/Details.jsx";
 import Searcher from "../Searcher/Searcher.jsx";
+import NotFound from "../NotFound/NotFound.jsx";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<List />}></Route>
         <Route path="movie/:id" element={<Details />}></Route>
-        <Route index path="search/:elect" element={<Searcher />}></Route>
+        <Route path="search/:elect" element={<Searcher />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
   );
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,14 @@
+import "./NotFound.css";
+
+const NotFound = () => {
+  return (
+    <>
+      <div className="notfound-cont">
+        <img src="/filmatic.png" alt="Not found image" />
+        <p>404 NOT FOUND</p>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
